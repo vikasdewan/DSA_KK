36c5031ee794dc0c02d7869cb4e0ad4362f260f7
VikasDewangan <dewanganvikas192@gmail.com>
2024-10-28 23:14:49 +0530

    formating documents

--- conditionalState/methods.java
+++ conditionalState/methods.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class methods {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // System.out.println("Q: enter two no. from user and print the sum of no. utill user say stop");
-        //  sum();
-        System.out.println("enter your age : " );
+        // System.out.println("Q: enter two no. from user and print the sum of no. utill
+        // user say stop");
+        // sum();
+        System.out.println("enter your age : ");
         int age = sc.nextInt();
         System.out.println(vote(age));
-
 
     }
 
@@ -18,15 +18,14 @@ public class methods {
         int a = sc.nextInt();
         System.out.println("enter the Second no. ");
         int b = sc.nextInt();
-        int c = a+b;
+        int c = a + b;
         System.out.println("sum is : " + c);
     }
-   
-    static String vote(int age){
-        if(age>=18){
+
+    static String vote(int age) {
+        if (age >= 18) {
             return "You can vote";
-        }
-        else{
+        } else {
             return "You cannot vote";
         }
     }
