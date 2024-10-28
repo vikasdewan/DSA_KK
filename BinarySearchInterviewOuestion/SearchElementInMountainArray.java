a7adc745a67d1c07fdaae9690e934dcbcefeddd9
VikasDewangan <dewanganvikas192@gmail.com>
2024-10-28 23:11:29 +0530

    making some changes in the searchelementinMountainArray.java

--- BinarySearchInterviewOuestion/SearchElementInMountainArray.java
+++ BinarySearchInterviewOuestion/SearchElementInMountainArray.java
@@ -2,45 +2,42 @@
 //not submitted now as it require some modification related to interface
 //change it later when learned OOP
 
-
 public class SearchElementInMountainArray {
     public static void main(String[] args) {
-        
+
     }
 
-    int search(int[] arr,int target){
+    int search(int[] arr, int target) {
         int peak = peakIndexInMountainArray(arr);
         int firstTry = oderAgnosticBS(arr, target, 0, peak);
-        if(firstTry !=-1){
+        if (firstTry != -1) {
             return firstTry;
         }
 
-        //try in second half
-        return oderAgnosticBS(arr, target, peak+1, arr.length-1);
+        // try in second half
+        return oderAgnosticBS(arr, target, peak + 1, arr.length - 1);
     }
 
-    public  int peakIndexInMountainArray(int [] arr){
+    public int peakIndexInMountainArray(int[] arr) {
         int start = 0;
-        int end =  arr.length - 1;
+        int end = arr.length - 1;
 
-        while(start<end){
+        while (start < end) {
             int mid = start + (end - start) / 2;
 
-            if(arr[mid] > arr[mid+1]){
-                //you are in dec part of mountain array
-            //this may be a ans but  we need to check for other part of array
-            // that is why end != mid-1;
-            //because we don't no that mid-1 element is small or greater than mid;
+            if (arr[mid] > arr[mid + 1]) {
+                // you are in dec part of mountain array
+                // this may be a ans but we need to check for other part of array
+                // that is why end != mid-1;
+                // because we don't no that mid-1 element is small or greater than mid;
                 end = mid;
+            } else {
+                start = mid + 1; // because we know that mid element is less than mid+1 element
             }
-            else{
-                start = mid+1; //because we know that mid element is less than mid+1 element
-            }
-            //in the end start == end , pointing to the peak element 
+            // in the end start == end , pointing to the peak element
         }
         return end;
-    } 
-
+    }
 
     static int oderAgnosticBS(int[] arr, int target, int start, int end) {
 
@@ -49,24 +46,21 @@ public class SearchElementInMountainArray {
         while (start <= end) {
             int mid = (start + end) / 2;
 
-             
             if (arr[mid] == target) {
                 return mid;
             }
-            
-            if(isAsc){
+
+            if (isAsc) {
                 if (target < arr[mid]) {
-                   end = mid - 1;
-               } else {
-                   start = mid + 1;
-               }
-            }
-            else{
-                if(target> arr[mid]){
-                    end = mid-1;
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
                 }
-                else{
-                    start = mid+1;
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
                 }
             }
 
