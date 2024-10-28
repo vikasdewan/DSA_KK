c53087f1466028f3b8c69bcb093f71a554b336ac
VikasDewangan <dewanganvikas192@gmail.com>
2024-10-28 23:21:14 +0530

    formating documents

--- linear Search/SearhInString.java
+++ linear Search/SearhInString.java
@@ -1,18 +1,19 @@
 import java.util.Arrays;
 
-public class SearhInString{
+public class SearhInString {
     public static void main(String[] args) {
         String name = "vikas";
         char target = 'k';
         // System.out.println(search(name,target));
         System.out.println(Arrays.toString(name.toCharArray()));
     }
-    static boolean search(String str ,char target){
-        if(str.length() == 0){
+
+    static boolean search(String str, char target) {
+        if (str.length() == 0) {
             return false;
         }
-        for(char ch : str.toCharArray()){
-            if(ch == target){
+        for (char ch : str.toCharArray()) {
+            if (ch == target) {
                 return true;
             }
         }
