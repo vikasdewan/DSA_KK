8c4b616b29fcda57581b389b51499acd7146a92e
VikasDewangan <dewanganvikas192@gmail.com>
2024-10-28 23:16:03 +0530

    formating documents

--- functions/scope.java
+++ functions/scope.java
@@ -14,15 +14,15 @@ public class scope {
                              //can make changes in the declared variables;          
                    int c = 50;//c is only known inside this block only;
             }
-                System.out.println(c);///give error: c's scope is only inside the block.
+              //  System.out.println(c);///give error: c's scope is only inside the block.
 
                 
             //below line give error as num not defined by the main function
-            System.out.println(num);
+          //  System.out.println(num);
     }
     static void random(){
         int num = 30;
-        System.out.println(a);//give error as a is not defined in the random() function.
+      //  System.out.println(a);//give error as a is not defined in the random() function.
         
 
 
