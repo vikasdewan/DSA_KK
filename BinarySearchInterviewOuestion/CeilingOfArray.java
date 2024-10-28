29231be7d859033971dab08db466a9c7f5fcd9eb
VikasDewangan <dewanganvikas192@gmail.com>
2024-10-28 23:12:04 +0530

    formating documents

--- BinarySearchInterviewOuestion/CeilingOfArray.java
+++ BinarySearchInterviewOuestion/CeilingOfArray.java
@@ -25,34 +25,30 @@ public class CeilingOfArray {
         return start;
     }
 
-
     public int[] searchRange(int[] nums, int target) {
-         
+
         int start = 0;
-        int end = nums.length-1;
+        int end = nums.length - 1;
 
-        while(start <= end){
-           int mid= (start + end)/2;
-            if(target == nums[mid]){
-                if(target == nums[mid-1]){
-                    return new int[]{mid-1,mid};
-                }
-                else if(target == nums[mid+1]){
-                    return new int[]{mid,mid+1};
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (target == nums[mid]) {
+                if (target == nums[mid - 1]) {
+                    return new int[] { mid - 1, mid };
+                } else if (target == nums[mid + 1]) {
+                    return new int[] { mid, mid + 1 };
                 }
             }
 
-            else if(target < nums[mid]){
-                end = mid-1;
-            }
-            else{
+            else if (target < nums[mid]) {
+                end = mid - 1;
+            } else {
                 start = mid + 1;
             }
         }
 
-        return new int[] {-1,-1};
+        return new int[] { -1, -1 };
     }
-
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
