f9d38b5af578eeab23873d1506ed54ab6c68e010
VikasDewangan <dewanganvikas192@gmail.com>
2024-10-28 23:12:59 +0530

    formating documents

--- BinarySearchInterviewOuestion/posOfElementInSortedInfiniteArray.java
+++ BinarySearchInterviewOuestion/posOfElementInSortedInfiniteArray.java
@@ -40,7 +40,7 @@ public class posOfElementInSortedInfiniteArray {
 
     public static void main(String[] args) {
 
-        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 23, 64, 67, 89,233,543,666,776,876,978 };
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 23, 64, 67, 89, 233, 543, 666, 776, 876, 978 };
 
         int target = 10;
 
