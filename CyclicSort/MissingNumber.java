7934a370ea2900a0dfcb5445ba54910a67b105d5
VikasDewangan <dewanganvikas192@gmail.com>
2025-03-24 13:01:09 +0530

    missing NUmber cyclic sort question

---
+++ CyclicSort/MissingNumber.java
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/missing-number/
+
+package CyclicSort;
+
+public class MissingNumber {
+    
+
+    public static void main(String[] args) {
+        int[] arr = {3,0,1};
+        System.out.println(missingNumber(arr));
+    }
+
+      static  public int missingNumber(int[] nums) {
+            int i=0;
+            while(i<nums.length){
+                int correctIdx = nums[i];
+                if(nums[i]<nums.length && nums[i] != nums[correctIdx]  ){
+                    int temp = nums[i];
+                    nums[i] = nums[correctIdx];
+                    nums[correctIdx] = temp;
+                }else{ 
+                    i++;
+                }
+            }
+    
+            for( i=0;i<nums.length;i++){
+                if(nums[i] != i){
+                    return i;
+                }
+            }
+    
+        return nums.length;
+        }
+    
+    }
+
