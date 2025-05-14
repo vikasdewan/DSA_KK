cb8a19367820572f03ab96eda1f78826efcdc2a8
VikasDewangan <dewanganvikas192@gmail.com>
2025-05-14 10:02:34 +0530

    `Added check for rotated sorted array in CheckArraySorted.java`

--- StriversDSA/Arrays/CheckArraySorted.java
+++ StriversDSA/Arrays/CheckArraySorted.java
@@ -4,7 +4,7 @@ public class CheckArraySorted {
     
     public static void main(String[] args) {
         
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {7,8,9,10,1,2,3,4,5,6};
         boolean isSorted = checkSorted(arr);
         if(isSorted){
             System.out.println("The array is sorted.");
@@ -14,13 +14,19 @@ public class CheckArraySorted {
     }
     
     public static boolean checkSorted(int[] arr){
+        //for rotated sorted array
+
+        int count = 0;
         int n = arr.length;
-        int pointer = 0;
-        while(pointer < n-1){
-            if(arr[pointer] > arr[pointer + 1]){
+        for(int i = 0; i < arr.length; i++){
+            // %n because whenever it goes at end index then 6 % 5 = 1 will start from index 1.
+            // and will not throw Array Index out of bound
+            if(arr[i] > arr[(i + 1) % n]){
+                count++;
+            }
+            if(count > 1){
                 return false;
             }
-            pointer++;
         }
         return true;
     }
