00a1ee4f2bcfda2e22e6d4668f9a50494c317401
Vikas Dewangan <138798400+vikasdewan@users.noreply.github.com>
2025-05-22 00:35:10 +0530

    Update AllPaths.java

--- Backtracking/AllPaths.java
+++ Backtracking/AllPaths.java
@@ -18,7 +18,7 @@ public class AllPaths {
 
     static void allPaths(String p,boolean[][] maze, int r, int c) {
 
-        if(r==maze.length-1 && c==maze[0].length-1){
+        if(r == maze.length-1 && c==maze[0].length-1){
             System.out.println(p);
             return;
         }
